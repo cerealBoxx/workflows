ac939eb0cb89d0dab6eba17870c0013199469be6
cerealBoxx <cookie.bra@gmail.com>
2015-01-23 21:50:00 +0200

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,9 @@ var jsSources = ['components/scripts/rclick.js',
 
 var sassSources = ['components/sass/style.scss'];
 
+var htmlSources = ['builds/development/*.html'];
+
+var jsonSources = ['builds/development/js/*.json'];
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
         .pipe(coffee({
@@ -45,12 +48,14 @@ gulp.task('compass', function() {
         .pipe(connect.reload());
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
 gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
@@ -58,4 +63,14 @@ gulp.task('connect', function() {
         root: 'builds/development/',
         livereload: true
     });
+});
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload());
+});
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+        .pipe(connect.reload());
 });
