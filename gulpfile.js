7095df3e8aa505dc4b5bd54b68a18f62ae0a4889
cerealBoxx <cookie.bra@gmail.com>
2015-01-23 21:23:56 +0200

    Added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
 
@@ -27,7 +28,8 @@ gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js/'));
+        .pipe(gulp.dest('builds/development/js/'))
+        .pipe(connect.reload());
 });
 
 
@@ -39,13 +41,21 @@ gulp.task('compass', function() {
             style: 'expanded'
         }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('builds/development/css'));
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
 
 gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    });
 });
